1f946d27fead174ad558956e05990f3ebf5a95b3
andrew <andrey.ashikhmin@gmail.com>
2019-03-07 20:57:22 +0000

    still trying to make travis happy

--- install-boost.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -ex
-
-wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz
-tar -xzvf boost_1_69_0.tar.gz
-cd boost_1_69_0
-./bootstrap.sh --prefix=/usr/local
-sudo ./b2 install
